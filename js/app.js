fda07dc0509cfc82165c574d2263cf6b505dfe1c
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-10-02 23:52:09 -0700

    js/app.js modified

--- js/app.js
+++ js/app.js
@@ -2,40 +2,40 @@
 
 
 var username = prompt('What is your name');
-console.log('Hi ' + username);
+console.log('welcome to my webpage ' + username);
+alert('Welcome to my webpage ! ' + username );
 
 var countryLocation = prompt('Am I orginally from Ethiopia ?').toLowerCase();
 if(countryLocation ==='yes'|| countryLocation ==='y' ) {
 
   console.log('yes!  I am orginally from Ethiopia');
   alert('yes! I am orginally from Ethiopia');
-}
+} else {alert('you get wrong');}
+
 var like = prompt('Am I like American football?').toLowerCase();
 if(like ==='yes'|| like ==='y') {
 
   console.log('yes! I do like American foot ball');
-  alert('yes! I do like American foot ball') ;
+  alert('yes! I do like American foot ball');
 
-}
+} else {alert('you get wrong');}
 
 var favorite = prompt('do you think software developer is my favorite field ? ').toLowerCase();
 if(favorite === 'yes' || favorite === 'y') {
   console.log('yes! It is my dream  profession  . so fare I would love to be building software instead of road and buliding ');
   alert('yes! It is my dream  profession . so fare I would love to be building software instead of road and buliding ');
-}
+} else {alert('you get wrong');}
 var fan = prompt('Am I Arsenal soccer team fan ? ').toLowerCase();
 if(fan === 'yes' || fan === 'y') {
   console.log('yes! I am Arsenal soccer team fun  since i was  age 20 , I never miss a single arsenal match ');
   alert('yes! I am Arsenal soccer team fun  since i was age 20, I never miss a single arsenal match ' );
-}
+} else {alert('you get wrong');}
 
 var have = prompt('Do I have a GMC yukon XLcar? ').toLowerCase();
 if(have === 'yes' || have === 'y') {
   console.log('yes! I have 2019 GMC Yukon XL ');
   alert('yes! I have 2019 GMC Yukon XL' );
-}
-
-
+} else {alert('you get wrong');}
 
 alert('Well done! , come back  next time to my webpage ' + username );
 
